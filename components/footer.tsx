5ea872af5a5444864d9545cd55d68db03c5781c6
Paul Chidiadi <paulchidiadi@gmail.com>
2024-09-28 17:13:17 +0100

    Make header and footer components responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,50 +4,51 @@ import Image from "next/image";
 export default function footer() {
   return (
     <>
-      <footer className="w-full h-fit bg-footer py-20 px-28">
-        <div className="flex items-start justify-between w-full">
+      <footer className="w-full h-fit bg-footer py-20 px-8 md:px-20 lg:px-28">
+        <div className="flex flex-col md:flex-row items-start justify-between w-full">
           <div className="flex-col items-start justify-between">
-            <h1 className="text-white text-2xl font-normal font-gothic mb-4 pb-3 border-b-4">
+            <h1 className="text-white md:text-lg lg:text-2xl font-normal font-gothic mb-4 pb-3 border-b-4">
               QUICK LINKS
             </h1>
             <div className="flex-col items-start justify-between gap-5 pl-3">
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 Home
               </h3>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 About Us
               </h3>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 Gallery
               </h3>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 Labari Prize
               </h3>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 Contact
               </h3>
             </div>
           </div>
           <div className="flex-col items-start justify-between">
-            <h1 className="text-white text-2xl font-normal font-gothic mb-4 pb-3 border-b-4">
+            <h1 className="text-white md:text-lg lg:text-2xl font-normal font-gothic mb-4 pb-3 border-b-4">
               LEGALS
             </h1>
             <div className="flex-col items-start justify-between gap-5 pl-3">
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 Terms and Agreement
               </h3>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 Privacy Policy
               </h3>
             </div>
           </div>
           <div className="flex-col items-start justify-between">
-            <h1 className="text-white text-2xl font-normal font-gothic mb-4 pb-3 border-b-4">
+            <h1 className="text-white md:text-lg lg:text-2xl font-normal font-gothic mb-4 pb-3 border-b-4">
               CONNECT
             </h1>
             <div className="flex-col items-start justify-between gap-5 pl-3">
               <div className="flex items-center justify-start gap-5 mb-4">
                 <Image
+                  className="hover:w-12 cursor-pointer"
                   src="/Group 64.png"
                   width={45}
                   height={45}
@@ -55,6 +56,7 @@ export default function footer() {
                   alt="instagram"
                 />
                 <Image
+                  className="hover:w-12 cursor-pointer"
                   src="/Group 61.png"
                   width={45}
                   height={45}
@@ -62,32 +64,33 @@ export default function footer() {
                   alt="twitter"
                 />
               </div>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 artxvibes@gmail.com
               </h3>
-              <h3 className="text-white text-xl font-normal font-lato mb-4">
+              <h3 className="text-white md:text-base lg:text-xl font-normal font-lato mb-4">
                 +2348186319784
               </h3>
             </div>
           </div>
         </div>
 
-        <div className="flex items-start justify-between w-full mt-14">
+        <div className="flex flex-col gap-5 items-start justify-between w-full mt-14 md:flex-row">
           <Image
+            className="w-28 md:w-48 lg:w-60"
             src="/arts-vibes-logo-2.png"
-            width={280}
+            width={240}
             height={120}
             quality={100}
             alt="Art and Vibes Logo"
           />
 
           <div className="flex-col items-center justify-start">
-            <h4 className="text-white text-xl font-normal font-lato mb-2">
+            <h4 className="text-white text-base lg:text-xl font-normal font-lato mb-2">
               We will love to reach you! Subscribe to get
             </h4>
             <div className="flex items-center justify-between w-full bg-white px-3 py-2 rounded-lg">
               <input
-                className=" flex-1 border-none outline-none px-5 py-2"
+                className=" flex-1 border-none outline-none pr-5 py-1"
                 type="email"
                 placeholder="Your Email..."
               />
@@ -98,15 +101,15 @@ export default function footer() {
           </div>
         </div>
 
-        <div className="flex items-start justify-between w-full mt-14">
-          <h4 className="text-white text-xl font-normal font-lato">
+        <div className="flex flex-col items-start justify-between w-full mt-14 lg:flex-row">
+          <h4 className="text-white text-base lg:text-xl font-normal font-lato">
             © 2024 ARTS&VIBES All rights reserved.
           </h4>
           <div className="flex items-center justify-between gap-8">
-            <h4 className="text-white text-xl font-normal font-lato">
+            <h4 className="text-white text-base lg:text-xl font-normal font-lato hover:text-primary cursor-pointer">
               Terms & Agreements
             </h4>
-            <h4 className="text-white text-xl font-normal font-lato">
+            <h4 className="text-white text-base lg:text-xl font-normal font-lato hover:text-primary cursor-pointer">
               Privacy Policy
             </h4>
           </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,41 +1,118 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
-      <nav className="relative flex items-center justify-between w-full px-20 py-4 bg-white rounded-b-3xl z-50">
+      <nav className="relative flex items-center justify-between w-full px-8 py-4 bg-white md:px-20  md:rounded-b-3xl z-50">
         <Image
-          className="w-20 h-auto md:w-24 lg:w-40"
+          className="w-24 h-auto md:w-24 lg:w-40"
           src="/arts-vibes-logo.png"
           width={100}
           height={100}
           quality={100}
           alt="Art and Vibes Logo"
         />
-        <div className="hidden lg:flex items-center justify-between w-2/5 font-lato font-medium text-faintBlack text-lg">
-          <a href="#" className="hover:text-black cursor-pointer">
+
+        {/* MENU ITEMS AT LARGE SCREEN */}
+        <div className="hidden lg:flex items-center justify-between w-2/5">
+          <a
+            href="#"
+            className="hover:text-black cursor-pointer font-lato font-medium text-faintBlack text-lg"
+          >
             Home
           </a>
-          <a href="#" className="hover:text-black cursor-pointer">
+          <a
+            href="#"
+            className="hover:text-black cursor-pointer font-lato font-medium text-faintBlack text-lg"
+          >
             About
           </a>
-          <a href="#" className="hover:text-black cursor-pointer">
+          <a
+            href="#"
+            className="hover:text-black cursor-pointer font-lato font-medium text-faintBlack text-lg"
+          >
             Events
           </a>
-          <a href="#" className="hover:text-black cursor-pointer">
+          <a
+            href="#"
+            className="hover:text-black cursor-pointer font-lato font-medium text-faintBlack text-lg"
+          >
             Gallery
           </a>
-          <a href="#" className="hover:text-black cursor-pointer">
+          <a
+            href="#"
+            className="hover:text-black cursor-pointer font-lato font-medium text-faintBlack text-lg"
+          >
             Labari Prize
           </a>
         </div>
+        {/* MENU ITEMS AT SMALL SCREEN */}
+        {showMenu && (
+          <div
+            className={`absolute flex flex-col items-start justify-start gap-3 w-full h-fit top-full left-0 py-4 px-8 md:px-20 bg-footer z-50 transition-all duration-5000 ease-linear transform ${
+              showMenu
+                ? "opacity-100 translate-y-"
+                : "opacity-0 translate-y-full"
+            } lg:hidden`}
+          >
+            <a
+              href="#"
+              className="hover:text-black cursor-pointer font-lato font-medium text-white text-base"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="hover:text-black cursor-pointer font-lato font-medium text-white text-base"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="hover:text-black cursor-pointer font-lato font-medium text-white text-base"
+            >
+              Events
+            </a>
+            <a
+              href="#"
+              className="hover:text-black cursor-pointer font-lato font-medium text-white text-base"
+            >
+              Gallery
+            </a>
+            <a
+              href="#"
+              className="hover:text-black cursor-pointer font-lato font-medium text-white text-base"
+            >
+              Labari Prize
+            </a>
+            <a
+              href="mailto:"
+              className="flex items-center justify-center bg-primary px-4 py-1 border-2 border-black rounded-lg font-lato font-medium text-black text-base cursor-pointer hover:text-faintBlack"
+            >
+              Contact
+            </a>
+          </div>
+        )}
+
         <a
           href="mailto:"
           className="hidden lg:flex items-center justify-center bg-primary px-4 py-1 border-2 border-black rounded-lg font-lato font-medium text-black text-lg cursor-pointer hover:text-faintBlack"
         >
           Contact
         </a>
+        {/* MENU BUTTONS */}
+        <div onClick={toggleMenu} className="lg:hidden">
+          {!showMenu && <i className="bx bx-menu text-2xl text-black"></i>}
+          {showMenu && <i className="bx bx-x text-2xl text-black"></i>}
+        </div>
       </nav>
     </>
   );
